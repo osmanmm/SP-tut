1516df4355173feebbe282bf1c56385ec96f01a2
osman <osman@mohameds-mini.ht.home>
2023-03-30 00:38:50 -0400

    dd

--- hr-demo/src/main/java/com/global/hr/EmployeeWS.java
+++ hr-demo/src/main/java/com/global/hr/EmployeeWS.java
@@ -1,5 +1,14 @@
 package com.global.hr;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeWS {
+@GetMapping("/employee")
+	public String getName() {
+		return "Hello";
+		
+	}
 
 }
