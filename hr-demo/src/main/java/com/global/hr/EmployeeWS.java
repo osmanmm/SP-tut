9d79c3f333b599c18c4d8c43ceccd46e6870ff89
osman <osman@localhost>
2023-03-30 23:02:35 -0400

    CommandLineRunner  , ApplicationRunner

--- hr-demo/src/main/java/com/global/hr/EmployeeWS.java
+++ hr-demo/src/main/java/com/global/hr/EmployeeWS.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeWS {
+	
 @GetMapping("/employee")
 	public String getName() {
 		return "Hello";
